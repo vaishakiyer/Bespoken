ad487331742f4052f9da84876a58f8c7db6007ba
Vaishak.Iyer <vaishak.iyer@tests-macbook-air.local>
2018-12-31 17:40:59 +0530

    some new changes

--- Bespoken/Utilities/Extension&Helper.swift
+++ Bespoken/Utilities/Extension&Helper.swift
@@ -190,3 +190,24 @@ extension StringProtocol where Index == String.Index {
     }
 }
 
+extension UISegmentedControl {
+    
+    func removeBorders() {
+      //  setBackgroundImage(imageWithColor(color: tintColor!), for: .normal, barMetrics: .default)
+        setBackgroundImage(imageWithColor(color: tintColor!), for: .selected, barMetrics: .default)
+        setDividerImage(imageWithColor(color: UIColor.clear), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+    }
+    
+    // create a 1x1 image with this color
+    private func imageWithColor(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0.0, y: 0.0, width:  1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor);
+        context!.fill(rect);
+        let image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return image!
+    }
+}
+
